8620c063c5da86b2a1dd65bf804f8088f6064dcc
nikzzxiter <nikzzxit@gmail.com>
2025-06-08 10:37:30 +0700

    Upload obfuscated test-obf.lua via Telegram Bot

---
+++ obfuscated/test-obf.lua
@@ -0,0 +1,53 @@
+--[[ Obfuscated by Nikzzx Lua Obfuscator v2.0 ]]
+--[[ Obfuscation Level: Extreme ]]
+
+local mzgfaeue = function() return 91 end
+local yrhusfes = '~VMh`9QY]_<:'
+
+local gqderwtm = 'LS0gRHVtbXkgTHVhIHNjcmlwdCAxS0IKCnByaW50'
+local bkrkgkpv = 'KCJTdGFydGluZyBMdWEgdGVzdC4uLiIpCgpmdW5j'
+local rtcrrqau = 'dGlvbiBmaWxsVGV4dCgpCiAgICBsb2NhbCB0ZXh0'
+local omtmjjna = 'ID0gIiIKICAgIGxvY2FsIGJhc2UgPSAiMTIzNDU2'
+local xzujtwer = 'Nzg5MGFiY2RlZiIKCiAgICBmb3IgaSA9IDEsIDUw'
+local kjhszawp = 'IGRvCiAgICAgICAgdGV4dCA9IHRleHQgLi4gYmFz'
+local prhvoarl = 'ZSAuLiAiIGxpbmUgIiAuLiBpIC4uICJcbiIKICAg'
+local aicrgjvl = 'IGVuZAoKICAgIHJldHVybiB0ZXh0CmVuZAoKbG9j'
+local uyprbxkr = 'YWwgZHVtbXkxID0gIlRlc3Qgc3RyaW5nIG9uZSBm'
+local fwpwnnls = 'b3IgTHVhIgpsb2NhbCBkdW1teTIgPSAiVGVzdCBz'
+local syfhscmp = 'dHJpbmcgdHdvIGZvciBMdWEiCmxvY2FsIGR1bW15'
+local ebmljzbx = 'MyA9ICJNb3JlIGZpbGxlciBkYXRhIGhlcmUiCgpm'
+local altvmelx = 'b3IgaSA9IDEsIDMwIGRvCiAgICBwcmludChkdW1t'
+local juvgminc = 'eTEpCiAgICBwcmludChkdW1teTIpCiAgICBwcmlu'
+local chnwyqyb = 'dChkdW1teTMpCmVuZAoKcHJpbnQoZmlsbFRleHQo'
+local gvwlvfgv = 'KSkK'
+
+--[[ Main Execution ]]
+local _ENV = _ENV or getfenv()
+local __ = {}
+
+function __.decode(s)
+    return (s:gsub('..', function(cc)
+        return string.char(tonumber(cc, 16))
+    end))
+end
+
+local _ = {}
+_[1] = __.decode(string.sub(loadstring('return _G.'..'fizjbwbr')(), 1, -1))
+_[2] = __.decode(string.sub(loadstring('return _G.'..'jdicythp')(), 1, -1))
+_[3] = __.decode(string.sub(loadstring('return _G.'..'ohhwjewv')(), 1, -1))
+_[4] = __.decode(string.sub(loadstring('return _G.'..'mqrgeact')(), 1, -1))
+_[5] = __.decode(string.sub(loadstring('return _G.'..'jjpwclkt')(), 1, -1))
+_[6] = __.decode(string.sub(loadstring('return _G.'..'hephyafu')(), 1, -1))
+_[7] = __.decode(string.sub(loadstring('return _G.'..'ciddebam')(), 1, -1))
+_[8] = __.decode(string.sub(loadstring('return _G.'..'ldtqxjhm')(), 1, -1))
+_[9] = __.decode(string.sub(loadstring('return _G.'..'mqifbpyy')(), 1, -1))
+_[10] = __.decode(string.sub(loadstring('return _G.'..'xhbwdaie')(), 1, -1))
+_[11] = __.decode(string.sub(loadstring('return _G.'..'xportreu')(), 1, -1))
+_[12] = __.decode(string.sub(loadstring('return _G.'..'uhqtfjyy')(), 1, -1))
+_[13] = __.decode(string.sub(loadstring('return _G.'..'gjkpgcgu')(), 1, -1))
+_[14] = __.decode(string.sub(loadstring('return _G.'..'mplxutjl')(), 1, -1))
+_[15] = __.decode(string.sub(loadstring('return _G.'..'jhuckxdn')(), 1, -1))
+_[16] = __.decode(string.sub(loadstring('return _G.'..'hwmvvxoa')(), 1, -1))
+local ___ = table.concat(_)
+local ____ = loadstring(___)
+if ____ then ____() end
