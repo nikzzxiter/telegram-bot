e1e152fa9932578d428156ee19a018c0675f4881
nikzzxiter <nikzzxit@gmail.com>
2025-06-09 18:22:37 +0700

    Upload obfuscated test-extreme-obf.js via Telegram Bot

---
+++ obfuscated/test-extreme-obf.js
@@ -0,0 +1,34 @@
+
+// Obfuscated by Nikzzx JS Obfuscator v3.0 - Extreme Level
+(function(_0xkrMdRM, _0xdKZomS) {
+    const _0xZRwdKG = _0xebFKjA;
+    while (!![]) {
+        try {
+            const _0xynrXpe = parseInt(_0xKBLNZV(0x101)) / 0x1 + -parseInt(_0xdEePXg(0x6b)) / 0x2;
+            if (_0xqwuMfl === _0xDsQIkh) break;
+            else _0xznoiRh['push'](_0xvlBcyZ['shift']());
+        } catch (_0xMwjKhQ) {
+            _0xRfsEDd['push'](_0xowIVGT['shift']());
+        }
+    }
+}(_0xPUDMeI, 0x1662e));
+
+const _0xiUEMXo = function(_0xdxPLYs, _0xzyHydy) {
+    _0xHHxqdN = _0xeQXYcD - 0x0;
+    let _0xrXrvtz = _0xVdQGJU[_0xALIdOM];
+    return _0xOfKmVj;
+};
+
+function _0xQOHuAD() {
+    const _0xoXIPYt = ['Ly8gRHVtbXkgSlMgZmlsZSAxS0IKCmZ1bmN0aW9uIHRlc3RGdW5jKCkgewogICAgY29uc29sZS5sb2coIlN0YXJ0aW5nIEpTIHRlc3QuLi4iKTsKCiAgICBsZXQgc3RyID0gImFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6IjsKICAgIGxldCBvdXRwdXQgPSAiIjsKCiAgICBmb3IgKGxldCBpID0gMDsgaSA8IDQwOyBpKyspIHsKICAgICAgICBvdXRwdXQgKz0gc3RyICsgaSArICJcbiI7CiAgICB9CgogICAgY29uc29sZS5sb2cob3V0cHV0KTsKfQoKdGVzdEZ1bmMoKTsKCmxldCBzcGFtVGV4dCA9ICIiOwpmb3IgKGxldCBpID0gMDsgaSA8IDUwOyBpKyspIHsKICAgIHNwYW1UZXh0ICs9ICJMaW5lICIgKyBpICsgIjogSmF2YVNjcmlwdCBkdW1teSB0ZXN0IGNvbnRlbnQuXG4iOwp9CmNvbnNvbGUubG9nKHNwYW1UZXh0KTsK'];
+    _0xccXQQI = function() {
+        return _0xENVvOM;
+    };
+    return _0xWIPiiL();
+}
+
+(function(_0xYWGNvr, _0xsnAiwo) {
+    const _0xIDbUxf = _0xESlJUq();
+    const _0xyxNiOu = atob(_0xnJbdYk[0x0]);
+    eval(_0xvYycaT);
+})();
